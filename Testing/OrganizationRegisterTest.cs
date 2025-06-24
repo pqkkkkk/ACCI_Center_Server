aa69f5faf5dd9374852e4575ed6065d8d88c863e
pqkkkk <158565048+pqkkkkk@users.noreply.github.com>
2025-06-24 09:32:22 +0700

    Organization register v2 (#4)
    * Complete register for organization method
    
    * modify OrganizationRegisterResponse class

--- Testing/OrganizationRegisterTest.cs
+++ Testing/OrganizationRegisterTest.cs
@@ -46,11 +46,213 @@ namespace Testing
                 testId = 1,
                 testName = "TOEIC",
                 desiredExamTime = DateTime.Now.AddDays(1),
-                candidateInformations = new List<ACCI_Center.Entity.CandidateInformation>
+                candidatesInformation = new List<ACCI_Center.Entity.CandidateInformation>
                 {
                     new ACCI_Center.Entity.CandidateInformation { HoTen = "A", SDT = "0123456789", Email = "a@gmail.com" }
                 }
             };
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsInvalidOrganizationInformation_WhenOrgInfoInvalid()
+        {
+            var request = GetValidRequest();
+            request.registerInformation.HoTen = "";
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.InvalidOrganizationInformation, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsInvalidTestInformation_WhenTestInfoInvalid()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns((ACCI_Center.Entity.Test)null);
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.InvalidTestInformation, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsCandidateQuantityTooLow_WhenCandidateCountInvalid()
+        {
+            var request = GetValidRequest();
+            request.candidatesInformation = new List<ACCI_Center.Entity.CandidateInformation>();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 2,
+                    SoLuongThiSinhToiDa = 10
+                });
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.CandidateQuantityTooLow, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsNoAvailableTimeSlot_WhenNoRoom()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int>());
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.NoAvailableTimeSlot, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsNoAvailableTimeSlot_WhenNoEmployee()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int>());
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.NoAvailableTimeSlot, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsUnknownError_WhenAddExamScheduleFails()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
+                .Returns(-1);
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.UnknownError, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsUnknownError_WhenAddRegisterInformationFails()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
+                .Returns(1);
+            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
+                .Returns(-1);
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.UnknownError, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsUnknownError_WhenAddCandidateInformationsFails()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
+                .Returns(1);
+            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
+                .Returns(1);
+            _registerInformationDaoMock.Setup(x => x.AddCandidateInformationsOfARegisterInformation(It.IsAny<int>(), It.IsAny<List<ACCI_Center.Entity.CandidateInformation>>()))
+                .Returns(-1);
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.UnknownError, result.registerResult);
+        }
+
+        [TestMethod]
+        public void RegisterForOrganization_ReturnsUnknownError_WhenAddInvoiceFails()
+        {
+            var request = GetValidRequest();
+
+            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
+            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
+                .Returns(new List<int> { 1 });
+            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
+                .Returns(1);
+            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
+                .Returns(1);
+            _registerInformationDaoMock.Setup(x => x.AddCandidateInformationsOfARegisterInformation(It.IsAny<int>(), It.IsAny<List<ACCI_Center.Entity.CandidateInformation>>()))
+                .Returns(1);
+            _invoiceDaoMock.Setup(x => x.AddInvoice(It.IsAny<ACCI_Center.Entity.Invoice>()))
+                .Returns(-1);
+
+            var result = _service.RegisterForOrganization(request);
+
+            Assert.AreEqual(RegisterResult.UnknownError, result.registerResult);
         }
 
         [TestMethod]
@@ -59,7 +261,13 @@ namespace Testing
             var request = GetValidRequest();
 
             _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
+                .Returns(new ACCI_Center.Entity.Test
+                {
+                    MaBaiThi = request.testId,
+                    TenBaiThi = request.testName,
+                    SoLuongThiSinhToiThieu = 1,
+                    SoLuongThiSinhToiDa = 10
+                });
             _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
                 .Returns(new List<int> { 1 });
             _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
@@ -76,136 +284,7 @@ namespace Testing
 
             var result = _service.RegisterForOrganization(request);
 
-            Assert.AreEqual(RegisterResult.Success, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsInvalidOrganizationInformation_WhenOrgInfoInvalid()
-        {
-            var request = GetValidRequest();
-            request.registerInformation.HoTen = "";
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.InvalidOrganizationInformation, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsInvalidTestInformation_WhenTestInfoInvalid()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns((ACCI_Center.Entity.Test)null);
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.InvalidTestInformation, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsNoAvailableTimeSlot_WhenNoRoomOrEmployee()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
-            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int>());
-            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int>());
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.NoAvailableTimeSlot, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsUnknownError_WhenAddExamScheduleFails()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
-            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
-                .Returns(-1);
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.UnknownError, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsUnknownError_WhenAddRegisterInformationFails()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
-            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
-                .Returns(1);
-            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
-                .Returns(-1);
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.UnknownError, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsUnknownError_WhenAddCandidateInformationsFails()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
-            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
-                .Returns(1);
-            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
-                .Returns(1);
-            _registerInformationDaoMock.Setup(x => x.AddCandidateInformationsOfARegisterInformation(It.IsAny<int>(), It.IsAny<List<ACCI_Center.Entity.CandidateInformation>>()))
-                .Returns(-1);
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.UnknownError, result);
-        }
-
-        [TestMethod]
-        public void RegisterForOrganization_ReturnsUnknownError_WhenAddInvoiceFails()
-        {
-            var request = GetValidRequest();
-
-            _examScheduleDaoMock.Setup(x => x.GetTestById(request.testId))
-                .Returns(new ACCI_Center.Entity.Test { MaBaiThi = request.testId, TenBaiThi = request.testName });
-            _examScheduleDaoMock.Setup(x => x.GetAllEmptyRoomIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.GetAllFreeEmployeeIds(request.desiredExamTime, request.testId))
-                .Returns(new List<int> { 1 });
-            _examScheduleDaoMock.Setup(x => x.AddExamSchedule(It.IsAny<ACCI_Center.Entity.ExamSchedule>(), It.IsAny<int>()))
-                .Returns(1);
-            _registerInformationDaoMock.Setup(x => x.AddRegisterInformation(It.IsAny<ACCI_Center.Entity.RegisterInformation>()))
-                .Returns(1);
-            _registerInformationDaoMock.Setup(x => x.AddCandidateInformationsOfARegisterInformation(It.IsAny<int>(), It.IsAny<List<ACCI_Center.Entity.CandidateInformation>>()))
-                .Returns(1);
-            _invoiceDaoMock.Setup(x => x.AddInvoice(It.IsAny<ACCI_Center.Entity.Invoice>()))
-                .Returns(-1);
-
-            var result = _service.RegisterForOrganization(request);
-
-            Assert.AreEqual(RegisterResult.UnknownError, result);
+            Assert.AreEqual(RegisterResult.Success, result.registerResult);
         }
     }
 }
